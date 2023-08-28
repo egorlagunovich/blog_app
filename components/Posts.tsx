5e5e3c6e906ad45d4301ce6169d60e125189ff66
egorlagunovich <egorlagunovich@gmail.com>
2023-08-28 12:23:47 +0200

    profile page

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export type PostProps = {
   postText: string;
@@ -11,9 +13,21 @@ export type PostProps = {
 };
 
 export const Post = ({ post }: PostProps) => {
-  console.log(post)
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
   return (
-    <Link href={`post/${post._id}`} className="bg-white rounded-lg p-5 cursor-pointer">
+    <Link
+      href={`post/${post._id}`}
+      className="bg-white rounded-lg p-5 cursor-pointer"
+    >
       <div className="flex flex-col justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
@@ -32,7 +46,9 @@ export const Post = ({ post }: PostProps) => {
           </p>
         </div>
         <div className="bg-white p-5 flex flex-col gap-3 rounded-lg">
-          <h1 className="font-bold text-4xl hover:text-blue-700">{post.title}</h1>
+          <h1 className="font-bold text-4xl hover:text-blue-700">
+            {post.title}
+          </h1>
           <div>Tags: {post.tags}</div>
         </div>
       </div>
